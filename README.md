4e3ab7f8f3f145eb138e7b7f9adbc223e5fd3a1f
webfliccy <f.elicity.anderson@gmail.com>
2023-03-20 12:08:17 +1100

    full project but with login error

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# co78c4675a6da142e82343e55
+# co10b41a9932640566664d554
 
 Warning: Vite enforces using jsx syntax inside jsx/tsx files, so it will complain about that. Solution: rename `.js` files to `.jsx` :)
 
--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,30 +1,42 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { Link, useLocation, useLoaderData, defer, Await } from "react-router-dom"
+import { getVans } from "../../api"
+
+export function loader({ params }) {
+    return defer({ van: getVans(params.id) })
+}
 
 export default function VanDetail() {
-    const params = useParams()
-    const [van, setVan] = React.useState(null)
+    const location = useLocation()
+    const loaderData = useLoaderData()
 
-    React.useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-            .then(res => res.json())
-            .then(data => setVan(data.vans))
-    }, [params.id])
+    const search = location.state ?.search || "";
+    const type = location.state ?.type || "all";
 
     return (
         <div className="van-detail-container">
-            {van ? (
-                <div className="van-detail">
-                    <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>
-                        {van.type}
-                    </i>
-                    <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>
-                </div>
-            ) : <h2>Loading...</h2>}
+            <Link
+                to={`..${search}`}
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to {type} vans</span></Link>
+            <React.Suspense fallback={<h2>Loading...</h2>}>
+                <Await resolve={loaderData.van}>
+                    {(van) => (
+                        <div className="van-detail">
+                            <img src={van.imageUrl} />
+                            <i className={`van-type ${van.type} selected`}>
+                                {van.type}
+                            </i>
+                            <h2>{van.name}</h2>
+                            <p className="van-price"><span>${van.price}</span>/day</p>
+                            <p>{van.description}</p>
+                            <button className="link-button">Rent this van</button>
+                        </div>
+                    )}
+                </Await>
+            </React.Suspense>
+
         </div>
     )
 }
